4ec1699be6eab3808b0c7311dc5ff50b50969e58
Nico Ismaili <nico@ismaili.de>
2022-06-16 17:55:10 +0200

    📝 Added Todo

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,8 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+	// TODO Begin this project anew by following this guide https://isamatov.com/create-vscode-extension-with-react/
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ese" is now active!');
@@ -23,4 +24,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
